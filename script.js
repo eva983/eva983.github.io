0234d23168d51b4b19413c5a13c423c85c64115c
eva <evapapazisi@gmail.com>
2024-01-31 18:22:59 +0200

    updates

--- script.js
+++ script.js
@@ -1,52 +1,16 @@
-// function myFunction() {
-//     var element = document.body;
-//     element.classList.toggle("light-mode");
-//     console.log("ok");
-//   }
 
+function toggleMenu() {
+  $('.nav-links').fadeToggle('fast');
+  $('body').removeClass('menu-open');
 
-// Iterate through the NodeList and add the 'styled-table' class to each table
-document.querySelectorAll('table').forEach(function(table) {
-  table.classList.add('styled-table');
-});  
-  let menuIcon=document.querySelector('#menu-icon');
-  let closeIcon=document.querySelector('#close-icon');
-  let navbar=document.querySelector('#navbar');
-  let menuBtns=document.querySelectorAll('.nav-btn');
-  console.log(menuBtns);
+}
 
-  menuIcon.onclick=()=> {
-    if (navbar.style.display === "block") {
-      navbar.style.display = "none";
-      document.body.style.overflow = 'visible'; //makes page scrollable
-
-
-    } else {
-      navbar.style.display = "block";
-      document.body.style.overflow = 'hidden'; //makes page unscrollable
-
-    }
+function closeMenu() {
+  if ($(window).width() <= 768) {
+      $('.nav-links').fadeOut('fast');
+      $('body').addClass('menu-open');
   }
-  menuBtns.forEach((btn) => btn.addEventListener("click", (e)=> {
-    navbar.style.display = "none";
-    console.log("k");
-    document.body.style.overflow = 'visible'; //makes page scrollable
-
-
-  })
-  );
-
-
- 
-
-  // const typed=new typed('.moving-text', {
-  //   strings: ['Web Developer'],
-  //   typeSpeed: 100,
-  //   backSpeed: 100,
-  //   backDelay:1000,
-  //   loop:true
-  // });
-
+}
   
   class TextScramble {
     constructor(el) {
@@ -55,7 +19,6 @@ document.querySelectorAll('table').forEach(function(table) {
       this.update = this.update.bind(this)
     }
     setText(newText) {
-      console.log(newText, this.el.innerText)
       const oldText = this.el.innerText
       const length = Math.max(oldText.length, newText.length)
       const promise = new Promise((resolve) => this.resolve = resolve)
